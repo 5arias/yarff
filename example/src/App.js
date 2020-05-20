37624c03e25502998881f44f46e3be5e08581641
Brody <17595659+5arias@users.noreply.github.com>
2020-05-20 18:28:01 -0400

    update naming

--- example/src/App.js
+++ example/src/App.js
@@ -4,7 +4,7 @@ import { ExampleComponent } from 'yarff'
 import 'yarff/dist/index.css'
 
 const App = () => {
-  return <ExampleComponent text="Create React Library Example 😄" />
+  return <ExampleComponent text="YARFF is Coming soon!" />
 }
 
 export default App
